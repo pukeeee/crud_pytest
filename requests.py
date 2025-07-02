1578d6c475a8cbdcbd27dc9ab3084535b8371365
pukeeee <pukee88@proton.me>
2025-07-02 20:28:03 +0300

    rename

--- requests.py
+++ requests.py
@@ -2,10 +2,10 @@ import requests
 
 url = "http://localhost:8000/users"
 data = {
-    "user_name": "Alice",
-    "email": "alice@mail.com",
+    "user_name": "Test",
+    "email": "test@mail.com",
     "password": "Password123/"
 }
-response = requests.post(url, json=data)
+response = requests.post(url, json = data)
 print(response.status_code)
 print(response.text)
